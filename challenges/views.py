48243360e525ba17515f3cd00f2fe33a63c03013
StudioCrew-design <dev.studiocrew@gmail.com>
2023-07-19 02:35:57 +0530

    Section 3: URL'S and Views completed

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
+
+monthly_challenges = {
+    "january": "This works in january",
+    "february": "This works in february",
+    "march": "This works in march",
+    "april": "This works in april",
+    "may": "This works in may",
+    "june": "This works in june",
+    "july": "This works in july",
+    "august": "This works in august",
+    "september": "This works in september",
+    "october": "This works in october",
+    "november": "This works in november",
+    "december": "This works in december",
+}
+
+
+def index(request):
+    months = list(monthly_challenges.keys())
+    list_items = ""
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+
+    return HttpResponseRedirect(redirect_path)
+#   return HttpResponseRedirect("/challenge/" + redirect_month)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
+    except:
+        return HttpResponseNotFound("<h1>This month is not supported</h1>")
